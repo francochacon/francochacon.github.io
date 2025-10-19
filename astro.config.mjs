aef23f9ee8c54fb471bb25bc5a11f8f8cdf78240
francochacon <ffranco2995@gmail.com>
2025-10-19 19:41:21 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
 
 export default defineConfig({
   site: "https://francochacon.github.io/",
   base: "/",
-  output: "static",
-  adapter: github(),
+  output: "static"
 });
