3ddb9b347f3cd2b67d02c203012ab7a96bdb7939
francochacon <ffranco2995@gmail.com>
2025-10-23 20:10:59 -0300

    Update const.js

--- src/const/const.js
+++ src/const/const.js
@@ -74,8 +74,7 @@ export const projects = [
     img: "../../../img/ecommerce-stripe-2.webp",
     title: "La minerìa en la Argentina",
     link: "proyecto1",
-    description: `BHP (ASX: BHP) is investing more than A$840 million ($555 million) in its Olympic Dam copper, gold and uranium complex 
-    in South Australia, advancing a series of growth projects ahead of a multibillion-dollar expansion decision expected in 2028.  `,
+    description: `Argentina cuenta con una rica diversidad de recursos minerales que posicionan al país como un actor relevante en la industria minera global. El país posee yacimientos de cobre, litio, oro, plata, zinc y otros minerales estratégicos, destacándose en regiones como la Puna, la Cordillera de los Andes y la Patagonia. Además, se aprovechan técnicas modernas de extracción y un marco regulatorio que fomenta la inversión sostenible.  `,
     technologies: [
       {
         icon: IconKeys.html,
