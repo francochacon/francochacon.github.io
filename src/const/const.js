96c2e53f58dc48f405e053e3fb7e4aeff33c5133
Franco Chacón <ffranco2995@gmail.com>
2026-06-11 19:59:45 -0300

    Update const.js

--- src/const/const.js
+++ src/const/const.js
@@ -9,26 +9,6 @@ export const studies = [
     corporation: "Facultad de Ingenieria, UNSa",
     date: "Nov 2025 – Present",
   },
-  {
-    title: "University Technician in Mining — Health & Safety",
-    corporation: "UPATECO",
-    date: "Jan 2026 – Present",
-  },
-  {
-    title: "Technical English for Mining Industry - A2",
-    corporation: "English For Mining",
-    date: "Jun 2025 – Sep 2025",
-  },
-  {
-    title: "Advanced Industrial Instrumentation",
-    corporation: "Fundacion YPF",
-    date: "Aug 2023 – Nov 2023",
-  },
-  {
-    title: "Mining Drilling & Mining Laboratory Techniques",
-    corporation: "VIOSOMOS",
-    date: "Feb 2026 – Apr 2026",
-  },
 ];
 
 export const IconKeys = {
