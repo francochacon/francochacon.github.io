23f04591a85af1456ae7a44d66da57cb422bb95e
Franco Chacón <ffranco2995@gmail.com>
2026-06-11 20:01:13 -0300

    Update const.js

--- src/const/const.js
+++ src/const/const.js
@@ -1,20 +1,76 @@
-export const studies = [
-  {
-    title: "Chemical Engineering",
-    corporation: "Universidad Tecnologica Nacional (UTN)",
-    date: "Jan 2016 – Oct 2023",
-  },
-  {
-    title: "Postgraduate — Mining Operations & Metallurgical Industry",
-    corporation: "Facultad de Ingenieria, UNSa",
-    date: "Nov 2025 – Present",
-  },
-];
-
 export const IconKeys = {
   html: "html",
   css: "css",
   javaScript: "javaScript",
   react: "react",
   astro: "astro",
-  materialUi: "mate
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+};
+
+export const skills = [
+  {
+    title: "Power BI",
+    icon: IconKeys.html,
+  },
+  {
+    title: "MYSQL",
+    icon: IconKeys.css,
+  },
+  {
+    title: "Python",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Autocad",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+];
+
+export const projects = [
+  {
+    img: "../../../img/ecommerce-stripe-2.webp",
+    title: "Exportación Record",
+    link: "proyecto2",
+    description: `Argentina cuenta con una rica diversidad de recursos minerales que posicionan al país como un actor relevante en la industria minera global. El país posee yacimientos de cobre, litio, oro, plata, zinc y otros minerales estratégicos, destacándose en regiones como la Puna, la Cordillera de los Andes y la Patagonia. Además, se aprovechan técnicas modernas de extracción y un marco regulatorio que fomenta la inversión sostenible.  `,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+    ],
+  },
+  {
+    img: "../../../img/ecommerce-stripe-2.webp",
+    title: "La minerìa en la Argentina",
+    link: "proyecto1",
+    description: `Argentina cuenta con una rica diversidad de recursos minerales que posicionan al país como un actor relevante en la industria minera global. El país posee yacimientos de cobre, litio, oro, plata, zinc y otros minerales estratégicos, destacándose en regiones como la Puna, la Cordillera de los Andes y la Patagonia. Además, se aprovechan técnicas modernas de extracción y un marco regulatorio que fomenta la inversión sostenible.  `,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+    ],
+  },
+];
+Era simplemente qu
